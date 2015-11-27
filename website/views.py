8f9d9a3b8703d0a3a2c30fd6e6850c1b1156ca3b
Nikolai Lazarov <nikolai.lazarov96@gmail.com>
2015-11-27 22:03:44 +0200

    testing database quering

--- website/views.py
+++ website/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from .models import Station
+from random import randint
 
 
 def index(request):
-    # stat = Station.objects.get(id=1)
-    return render(request, 'website/index.html')
+    stat = Station.objects.get(id=randint(1, 1000))
+    return render(request, 'website/index.html', locals())
