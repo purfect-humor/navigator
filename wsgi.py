016a49d58ef8751669b1fdbec4c1feace21386f4
Nikolai Lazarov <nikolai.lazarov96@gmail.com>
2015-11-30 21:40:24 +0200

    removes wsgi.py

--- wsgi.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "navigator.settings")
-from django.core.wsgi import get_wsgi_application
-
-application = get_wsgi_application()
