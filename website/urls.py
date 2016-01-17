7cc48a06faecd17c00e9ee9511b851984cee502c
Nikolai Lazarov <nikolai.lazarov96@gmail.com>
2016-01-17 20:00:46 +0200

    adds rest method

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from .views import index
+from .views import *
 
 urlpatterns = [
+    url(r'^getStationById/([0-9]+)', getStationById),
     url(r'^$', index)
 ]
--- website/views.py
+++ website/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from .models import Station
 from random import randint
+from django.http import JsonResponse
 
 
 def index(request):
-    stat = Station.objects.get(id=randint(1, 1000))
+    #stat = Station.objects.get(id=randint(1, 1000))
     return render(request, 'website/index.html', locals())
+
+
+def getStationById(request, id):
+    stat = Station.objects.get(id=id)
+    return JsonResponse({
+        'number': stat.number,
+        'name': stat.name,
+        'latitude': stat.latitude,
+        'longitude': stat.longitude,
+    })
